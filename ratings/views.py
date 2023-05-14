71257dfd6e498e353cf1e458cd42227d6b6b0bf2
KONSTANTINOS <kostasfergadis74@gmail.com>
2023-05-14 22:32:22 +0300

    ratingchange

--- ratings/views.py
+++ ratings/views.py
@@ -8,42 +8,6 @@ from .models import Rating
 
 
 class Like(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request, pk):
-        user = request.user
-        try:
-            group = Group.objects.get(pk=pk)
-        except Group.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        rating = Rating.objects.get(group=group, user=user)
-        if not rating.has_liked:
-
-            rating.likes += 1
-            rating.has_liked = True
-
-            if rating.has_disliked:
-                rating.has_disliked = False
-                rating.dislikes -= 1
-
-                rating.save()
-                group.likes += 1
-                group.dislikes -= 1 if rating.has_disliked else 0
-                group.likes += 1
-                group.save()
-
-                return Response({'message': 'Liked group'}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({'error': 'You have already liked this group'}, status=status.HTTP_400_BAD_REQUEST)
-        # return the updated user likes count
-        # user_likes = rating.likes if hasattr(rating, 'likes') else 0
-        # return Response({'user_likes': user_likes, 'group_likes': group.likes}, status=status.HTTP_200_OK)
-
-
-class Dislike(APIView):
-    permission_classes = (IsAuthenticated,)
-
     def post(self, request, pk):
         user = request.user
         try:
@@ -54,19 +18,48 @@ class Dislike(APIView):
         # check if the user has already rated this group
         try:
             rating = Rating.objects.get(group=group, user=user)
-            if rating.has_disliked:
-                return Response({'error': 'You have already disliked this group'}, status=status.HTTP_400_BAD_REQUEST)
-            rating.dislikes += 1
-            rating.has_disliked = True
-            rating.save()
+            if rating.has_liked:
+                # User has already liked this group, return an error
+                return Response({'error': 'You have already liked this group'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                # User has not yet rated this group, set has_liked to True
+                rating.likes
+                rating.has_liked = True
+                rating.save()
         except Rating.DoesNotExist:
+            # User has not yet rated this group, create a new rating instance and set has_liked to True
             rating = Rating.objects.create(
-                group=group, user=user, dislikes=1, has_disliked=True)
+                group=group, user=user, has_liked=True)
 
-        # update the group likes count
-        group.likes += 1
+
+class Dislike(APIView):
+    def post(self, request, pk):
+        user = request.user
+        try:
+            group = Group.objects.get(pk=pk)
+        except Group.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # Check if the user has already rated this group
+        try:
+            rating = Rating.objects.get(group=group, user=user)
+            if rating.has_disliked:
+                # User has already disliked this group, return an error
+                return Response({'error': 'You have already disliked this group'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                # User has not yet rated this group, set has_disliked to True
+                rating.has_disliked = True
+                rating.save()
+        except Rating.DoesNotExist:
+            # User has not yet rated this group, create a new rating instance and set has_disliked to True
+            rating = Rating.objects.create(
+                group=group, user=user, has_disliked=True)
+
+        # Update the likes and dislikes count of the group
+        group.dislikes += 1
+        if rating.has_liked:
+            group.likes -= 1
         group.save()
 
-        # return the updated user likes count
-        user_dislikes = rating.dislikes if hasattr(rating, 'dislikes') else 0
-        return Response({'user_likes': user_dislikes, 'group_likes': group.dislikes}, status=status.HTTP_200_OK)
+        # Return the updated likes and dislikes count of the group
+        return Response({'likes': group.likes, 'dislikes': group.dislikes}, status=status.HTTP_200_OK)
